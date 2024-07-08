4580a00099a2226b6ab2b3dbc6cfaada45b9fda2
Tales Anaximandro do Bonfim Visgueira <talesvisgueira@gmail.com>
2024-07-08 00:29:57 -0300

    Endpoint para product
    Inclusão do endpoint e teste para product

---
+++ src/domain/product/entity/product.interface.ts
@@ -0,0 +1,11 @@
+export default interface ProductInterface {
+  get id(): string;
+  get name(): string;
+  get price(): number;
+
+  validate(): boolean;
+
+  changeName(name: string): void ;
+  changePrice(price: number): void ;
+  
+}
